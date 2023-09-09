c884b6362b125870ca79b5059fa8cdbafcc1e0bb
marco <franzonmarco94@gmail.com>
2023-09-09 19:32:41 +0200

    clean comments

--- benchmarks/matmul/results.py
+++ benchmarks/matmul/results.py
@@ -1,31 +1,28 @@
 import matplotlib.pyplot as plt
 
-# Define two empty lists to store the values
 py_odd_lines_list = []
 py_even_lines_list = []
 
 mj_odd_lines_list = []
 mj_even_lines_list = []
 
-# Read the first file
 with open("matmul/py.perf", "r") as file:
     lines = file.readlines()
     for i, line in enumerate(lines):
-        if i % 2 == 0:  # Even lines
+        if i % 2 == 0:
             py_even_lines_list.append(float(line.strip()))
-        else:  # Odd lines
+        else:
             py_odd_lines_list.append(float(line.strip()))
 
-# Read the second file
 with open("matmul/mj.perf", "r") as file:
     lines = file.readlines()
     for i, line in enumerate(lines):
-        if i % 2 == 0:  # Even lines
+        if i % 2 == 0:
             mj_even_lines_list.append(float(line.strip()))
-        else:  # Odd lines
+        else:
             mj_odd_lines_list.append(float(line.strip()))
 
-# Create a plot
+# plot
 plt.figure(1)
 plt.subplot(211)
 plt.plot(py_odd_lines_list, label="Python")
@@ -48,6 +45,4 @@ plt.xticks(default_x_ticks, x)
 plt.xlabel("Input")
 plt.ylabel("GFLOP/s")
 
-
-# Show the plot
 plt.savefig("matmul/plot_time_gflops.png")
